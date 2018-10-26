4766332b7e0c89406285f0ae6564442e501170a8
Harbor Young <yhb241@163.com>
2018-10-26 23:37:25 +0800

    Change the JavaScript code style

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,14 +1,14 @@
 <script>
 export default {
-  created{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+  created () {
     // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    logs.unshift(Date.now()){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    wx.setStorageSync('logs', logs){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    const logs = wx.getStorageSync('logs') || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync('logs', logs);
 
-    console.log('app created and cache logs by setStorageSync'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    console.log('app created and cache logs by setStorageSync');
+  }
+};
 </script>
 
 <style>
