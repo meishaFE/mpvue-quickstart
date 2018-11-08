8e34bdc9e8e912478b157bb5e5f96fc898005c3a
Harbor Young <yhb241@163.com>
2018-11-08 22:20:20 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -3,10 +3,24 @@
 // mpvue has been set in webpack.base.conf with an alias.
 {{/if_eq}}
 import Vue from 'vue';
-import App from './App';
+import App from '@/App';
+import store from '@/store';
+import _ from 'lodash';
+import * as $utils from '@/utils/index';
+import { fly as $http } from '@/utils/fetch';
+import { ENV, API } from '@/config/api';
+
+Vue.prototype._ = _;
+Vue.prototype.$utils = $utils;
+Vue.prototype.$http = $http;
+Vue.prototype.$ENV = ENV;
+Vue.prototype.$API = API;
 
 Vue.config.productionTip = false;
 App.mpType = 'app';
 
-const app = new Vue(App);
+const app = new Vue({
+  store,
+  ...App
+});
 app.$mount();
